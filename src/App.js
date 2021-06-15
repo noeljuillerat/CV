b044c5e3da2c8c6934e97e1886f9404a5890f9e6
noeljuillerat <noel.juillerat@gmail.com>
2021-06-15 10:25:03 +0200

    toujours image !

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Knowledges from "./pages/Knowledges";
 import Contact from "./pages/Contact";
@@ -9,7 +9,7 @@ import NotFound from "./pages/NotFound";
 const App = () => {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/competences" component={Knowledges} />
@@ -17,7 +17,7 @@ const App = () => {
           <Route path="/contact" component={Contact} />
           <Route component={NotFound} />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 };
